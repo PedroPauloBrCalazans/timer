59a9c6d94787bd498f3efcd891455aeb9081e508
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2023-07-07 19:34:54 -0300

    feat: Add nova lib + add de novas rotas.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { Router } from "./Router";
+import { BrowserRouter } from "react-router-dom";
+
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
@@ -5,8 +8,9 @@ import { GlobalStyle } from "./styles/global";
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <button></button>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );
