49609e8ad52f113daec778cac4ec9a348c654cd8
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2023-07-08 23:16:47 -0300

    feat: Add layout default + rotas.

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,26 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { History } from "./pages/History";
+import { DefaultLayout } from "./layouts/DefaultLayout";
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   );
+}
+
+{
+  /*   <Route path="/admin" element={<AdminLayout />}>
+        <Route path="/products"  />
+      </Route> 
+      AQUI POSSO TER OUTRO LAYOUT PARA UM PAINEL DE ADMIN
+      LOCALHOST:3000/ADMIN/PRODUCTS  VAI SOMANDO OS PATH
+
+      componente de DefaultLayout aplicar o layout em todas as rotas
+*/
 }
